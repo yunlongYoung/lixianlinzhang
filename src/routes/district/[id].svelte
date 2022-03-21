af5743ed7bae730cb2d294d7d6433da9b0a39176
yunlongYoung <dragonlip@qq.com>
2022-03-21 12:52:59 +0800

    因为从静态转换为动态后无法加载地图，加载最基本的动态图

--- src/routes/district/[id].svelte
+++ src/routes/district/[id].svelte
@@ -7,27 +7,6 @@
 		map = new T.Map('mapDiv');
 		let [x, y] = district.center.split(',');
 		map.centerAndZoom(new T.LngLat(x, y), zoom);
-		//设置显示地图的中心点和级别
-		//允许鼠标滚轮缩放地图
-		map.enableScrollWheelZoom();
-
-		var imageURL =
-			'http://t0.tianditu.gov.cn/img_w/wmts?' +
-			'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles' +
-			'&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=d999b17e082a319d7892134e07b054af';
-		//创建自定义图层对象
-		var lay = new T.TileLayer(imageURL, { minZoom: 13, maxZoom: 19 });
-		//将图层增加到地图上
-		map.addLayer(lay);
-
-		var imageURL2 =
-			'http://t0.tianditu.gov.cn/cia_w/wmts?' +
-			'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cia&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles' +
-			'&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=d999b17e082a319d7892134e07b054af';
-		//创建自定义图层对象
-		var lay2 = new T.TileLayer(imageURL2, { minZoom: 13, maxZoom: 19 });
-		//将图层增加到地图上
-		map.addLayer(lay2);
 	});
 </script>
 
