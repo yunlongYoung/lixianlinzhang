33c36864e6d0cf377917228d7986ab862f16da60
yunlongYoung <dragonlip@qq.com>
2022-03-21 21:54:33 +0800

    卫星图加标注

--- src/routes/district/[id].svelte
+++ src/routes/district/[id].svelte
@@ -1,14 +1,21 @@
 <script>
 	import { onMount } from 'svelte';
+	import { all_points } from '$lib/points';
 	export let district;
 	var map;
 	var zoom = 15;
 	var lay;
+	var lay2;
 	var onlyMapLay;
+	var points;
 	onMount(async () => {
 		var imageURL =
 			'http://t0.tianditu.gov.cn/img_w/wmts?' +
 			'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles' +
+			'&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=d999b17e082a319d7892134e07b054af';
+		var imageURL2 =
+			'http://t0.tianditu.gov.cn/cia_w/wmts?' +
+			'SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cia&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles' +
 			'&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=d999b17e082a319d7892134e07b054af';
 		// var imageURL =
 		// 	'http://t0.tianditu.gov.cn/img_w/wmts?' +
@@ -16,7 +23,8 @@
 		// 	'&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=d999b17e082a319d7892134e07b054af';
 		//创建自定义图层对象
 		lay = new T.TileLayer(imageURL, { minZoom: 13, maxZoom: 18 });
-		var config = { layers: [lay] };
+		lay2 = new T.TileLayer(imageURL2, { minZoom: 13, maxZoom: 18 });
+		var config = { layers: [lay, lay2] };
 		//初始化地图对象
 		map = new T.Map('mapDiv', config);
 		//设置显示地图的中心点和级别
@@ -39,6 +47,6 @@
 <style>
 	#mapDiv {
 		width: 100%;
-		height: 400px;
+		height: 720px;
 	}
 </style>
