abf62a40af590700bbb27558e8f937d362057964
yunlongYoung <dragonlip@qq.com>
2022-03-27 21:13:48 +0800

    县、乡、村采用不同的缩放率

--- src/routes/district/[id].svelte
+++ src/routes/district/[id].svelte
@@ -4,4 +4,10 @@
 </script>
 
 <h1>{district.name}</h1>
+{#if district.id.slice(-7)=='0000000'}
+<img src={`http://api.tianditu.gov.cn/staticimage?center=${district.center}&width=480&height=720&zoom=12&layers=%20img_c,cva_c&tk=d999b17e082a319d7892134e07b054af`} alt={district.name}>
+{:else if district.id.slice(-6)=='000000'}
+<img src={`http://api.tianditu.gov.cn/staticimage?center=${district.center}&width=480&height=720&zoom=13&layers=%20img_c,cva_c&tk=d999b17e082a319d7892134e07b054af`} alt={district.name}>
+{:else if district.id.slice(-3)=='000'}
 <img src={`http://api.tianditu.gov.cn/staticimage?center=${district.center}&width=480&height=720&zoom=14&layers=%20img_c,cva_c&tk=d999b17e082a319d7892134e07b054af`} alt={district.name}>
+{/if}
