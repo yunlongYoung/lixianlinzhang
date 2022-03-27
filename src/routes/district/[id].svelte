6d4973d48a5149669235b1d3effd1325776a2e11
yunlongYoung <dragonlip@qq.com>
2022-03-27 20:38:39 +0800

    修改静态图片高宽为720*480

--- src/routes/district/[id].svelte
+++ src/routes/district/[id].svelte
@@ -4,4 +4,4 @@
 </script>
 
 <h1>{district.name}</h1>
-<img src={`http://api.tianditu.gov.cn/staticimage?center=${district.center}&width=720&height=1080&zoom=14&layers=%20img_c,cva_c&tk=d999b17e082a319d7892134e07b054af`} alt={district.name}>
+<img src={`http://api.tianditu.gov.cn/staticimage?center=${district.center}&width=480&height=720&zoom=14&layers=%20img_c,cva_c&tk=d999b17e082a319d7892134e07b054af`} alt={district.name}>
