0a4fb8d4f9d87e61ae4fbfc81a7b600c47d4c618
yunlongYoung <dragonlip@qq.com>
2022-04-19 14:37:57 +0800

    重新恢复之前乡的中心坐标，还有县乡不同的缩放率

--- src/routes/district/[id].svelte
+++ src/routes/district/[id].svelte
@@ -4,16 +4,19 @@
 </script>
 
 <h1>{district.name}</h1>
-<div id="mapDiv">
+{#if district.id.slice(-6) == '000000'}
+	<img
+		src={`http://api.tianditu.gov.cn/staticimage?center=${district.center}&width=360&height=640&zoom=10&layers=%20img_c,cva_c&tk=d999b17e082a319d7892134e07b054af`}
+		alt={district.name}
+	/>
+{:else if district.id.slice(-3) == '000'}
+	<img
+		src={`http://api.tianditu.gov.cn/staticimage?center=${district.center}&width=360&height=640&zoom=12&layers=%20img_c,cva_c&tk=d999b17e082a319d7892134e07b054af`}
+		alt={district.name}
+	/>
+{:else}
 	<img
 		src={`http://api.tianditu.gov.cn/staticimage?center=${district.center}&width=360&height=640&zoom=14&layers=%20img_c,cva_c&tk=d999b17e082a319d7892134e07b054af`}
 		alt={district.name}
 	/>
-</div>
-
-<style>
-	#mapDiv {
-		width: 100%;
-		height: 720px;
-	}
-</style>
+{/if}
