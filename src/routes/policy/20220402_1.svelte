641fcb59661a8d4faafdbd9d57df2d4af4f7e3f4
yunlongYoung <dragonlip@qq.com>
2022-04-21 08:01:42 +0800

    修改完善静态资源路径

--- src/routes/policy/20220402_1.svelte
+++ src/routes/policy/20220402_1.svelte
@@ -2,10 +2,10 @@
 <em> 2022年4月2日 </em>
 <div class="pure-g">
 	<div class="pure-u-1">
-		<img src="../../static/pictures/20220402-1.jpg" alt="1" />
-		<img src="../../static/pictures/20220402-2.jpg" alt="2" />
-		<img src="../../static/pictures/20220402-3.jpg" alt="3" />
-		<img src="../../static/pictures/20220402-4.jpg" alt="4" />
+		<img src="/pictures/202204/20220402-1.jpg" alt="1" />
+		<img src="/pictures/202204/20220402-2.jpg" alt="2" />
+		<img src="/pictures/202204/20220402-3.jpg" alt="3" />
+		<img src="/pictures/202204/20220402-4.jpg" alt="4" />
 	</div>
 </div>
 
