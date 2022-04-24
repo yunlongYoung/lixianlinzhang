3bf35240be9b5e14faa0b0479fdd25549e0d92b5
yunlongYoung <dragonlip@qq.com>
2022-04-24 10:55:44 +0800

    修正政策列表格式，缩小规划标题字号

--- src/routes/policy/index.svelte
+++ src/routes/policy/index.svelte
@@ -4,23 +4,29 @@
 
 <h1>政策文件</h1>
 <div class="pure-g">
-	{#each articles as { link, title }, i}
-		<ul>
-			<div class="pure-u-5-24">
-				<span class="date">{link.slice(0, 4) + '年'}</span>
-				<br />
-				<span class="date"
-					>{parseInt(link.slice(5, 7)) + '月' + parseInt(link.slice(8, -2)) + '日'}</span
-				>
-			</div>
-			<div class="pure-u-19-24">
-				<a href={'policy/' + link}>{title}</a>
-			</div>
-		</ul>
-	{/each}
+	<ul>
+		{#each articles as { link, title }, i}
+			<li>
+				<div class="pure-u-5-24">
+					<span class="date">{link.slice(0, 4) + '年'}</span>
+					<br />
+					<span class="date"
+						>{parseInt(link.slice(5, 7)) + '月' + parseInt(link.slice(8, -2)) + '日'}</span
+					>
+				</div>
+				<div class="pure-u-19-24">
+					<a href={'policy/' + link}>{title}</a>
+				</div>
+			</li>
+		{/each}
+	</ul>
 </div>
 
 <style>
+	li{
+		margin: 1rem;
+		list-style-type: none;
+	}
 	.date {
 		font-size: 0.5rem;
 	}
